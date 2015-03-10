bc1c80b0b63208b4998f49594eb163cbda27629b
Darkfire16 <gmodman12@gmail.com>
2015-03-10 02:23:27 -0400

    50% chance of any drop now, and air sound when surfacing.

--- NuclearLiberation/Enemy.cpp
+++ NuclearLiberation/Enemy.cpp
@@ -54,33 +54,34 @@ void Enemy::update(float dt)
 
 void Enemy::onDeath() {
 	float h = 1 - getPosition().y/game->worldSize.y;
+	bool flip = rand()%2;
 	int rander = random(3);
 	switch(rander) {
 	case 1:
 		game->audio->playCue(EXP1);
-		if(rand()%100 <= h*100)
+		if(rand()%100 <= h*100 && flip)
 			game->spawnAir(getPosition(), Vector3(0,1,0));
-		else if(rand()%100 == h*100)
+		else if(rand()%100 == h*100 && flip)
 			game->spawnShield(getPosition(), Vector3(0,0,0));
-		else if(rand()%100 == h*100)
+		else if(rand()%100 == h*100 && flip)
 			game->spawnPower(getPosition(), Vector3(0,-2,0));
 		break;
 	case 2:
 		game->audio->playCue(EXP2);
-		if(rand()%100 <= h*100)
+		if(rand()%100 <= h*100 && flip)
 			game->spawnPower(getPosition(), Vector3(0,1,0));
-		else if(rand()%100 == h*100)
+		else if(rand()%100 == h*100 && flip)
 			game->spawnAir(getPosition(), Vector3(0,0,0));
-		else if(rand()%100 == h*100)
+		else if(rand()%100 == h*100 && flip)
 			game->spawnShield(getPosition(), Vector3(0,-2,0));
 		break;
 	case 3:
 		game->audio->playCue(EXP3);
-		if(rand()%100 <= h*100)
+		if(rand()%100 <= h*100 && flip)
 			game->spawnShield(getPosition(), Vector3(0,1,0));
-		else if(rand()%100 == h*100)
+		else if(rand()%100 == h*100 && flip)
 			game->spawnPower(getPosition(), Vector3(0,0,0));
-		else if(rand()%100 == h*100)
+		else if(rand()%100 == h*100 && flip)
 			game->spawnAir(getPosition(), Vector3(0,-2,0));
 		break;
 	default:
