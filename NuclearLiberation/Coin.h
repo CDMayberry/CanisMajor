cd6b5f3e53111acafbb93963befff8978b761421
JSybrandt <nalta45@gmail.com>
2015-03-20 14:15:33 -0400

    Revert "Merge branch 'master' of https://github.com/CDMayberry/CanisMajor"
    This reverts commit 5ee2c5e467bf1e8bb67ab00b4499531c07c2396f, reversing
    changes made to 47888235ec878527744f03bb27150b503570fb34.

--- NuclearLiberation/Coin.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Geometry.h"
-class Coin : public virtual Geometry
-{
-public:
-	void init(ID3D10Device* device, D3DXCOLOR color = WHITE);
-
-};
--- NuclearLiberation/mathUtil.h
+++
@@ -1,9 +0,0 @@
-#include "constants.h"
-#include "d3dUtil.h"
-#include <random>
-
-Vector3 rotateX(Vector3 v, float deg);
-Vector3 rotateY(Vector3 v, float deg);
-Vector3 rotateZ(Vector3 v, float deg);
-
-int random(int num);
